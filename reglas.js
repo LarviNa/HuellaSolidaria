500115e63692c83f561ba02929b0cab83840a219
LarviNA <mbarraza193@gmail.com>
2025-09-02 20:17:49 -0400

    reglas

---
+++ reglas.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  
+  // Esto inicializa los tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Aquí puedes agregar más código JavaScript en el futuro
+
+});
